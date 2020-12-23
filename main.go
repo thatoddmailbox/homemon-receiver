f217fd06d33c4fb477ce2439b49576a0f2fde102
Alex Studer <alex@studer.dev>
2020-12-23 18:53:34 -0500

    add flag to generate a new, random token

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"flag"
+	"fmt"
 	"log"
 )
 
 func main() {
+	generateToken := flag.Bool("generate-token", false, "Generate a random token and exit.")
+	flag.Parse()
+	if *generateToken {
+		newToken := make([]byte, 64)
+		_, err := rand.Read(newToken)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(base64.URLEncoding.EncodeToString(newToken))
+		return
+	}
+
 	log.Println("homemon-receiver")
 
 	err := loadConfig()
